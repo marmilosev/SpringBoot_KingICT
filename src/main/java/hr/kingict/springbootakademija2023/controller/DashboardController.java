5dbf9394434a64a3cb8e3e5d9b4bbe9f61f369a6
marmilosev <mmilosevic1411@gmail.com>
2023-10-21 10:14:44 +0200

    controller for getting html file

--- src/main/java/hr/kingict/springbootakademija2023/controller/DashboardController.java
+++ src/main/java/hr/kingict/springbootakademija2023/controller/DashboardController.java
@@ -2,8 +2,6 @@ package hr.kingict.springbootakademija2023.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class DashboardController {
