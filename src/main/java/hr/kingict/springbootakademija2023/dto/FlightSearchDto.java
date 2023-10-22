5c2061642846dadeea76e1e525f1344c2cbe3b6c
marmilosev <mmilosevic1411@gmail.com>
2023-10-22 10:57:30 +0200

    update dto FlightSearchDto class

--- src/main/java/hr/kingict/springbootakademija2023/dto/FlightSearchDto.java
+++ src/main/java/hr/kingict/springbootakademija2023/dto/FlightSearchDto.java
@@ -1,13 +1,99 @@
 package hr.kingict.springbootakademija2023.dto;
 
 public class FlightSearchDto {
-    private String name;
 
-    public String getName() {
-        return name;
+    //outbound data
+    private String outboundDepartureAirport;
+    private String outboundDepartureDate;
+    private String outboundArrivalAirport;
+    private String outboundArrivalDate;
+
+    //inbound data
+    private String inboundDepartureAirport;
+    private String inboundDepartureDate;
+    private String inboundArrivalAirport;
+    private String inboundArrivalDate;
+
+    private String price;
+    private String carrier;
+
+    public String getOutboundDepartureAirport() {
+        return outboundDepartureAirport;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setOutboundDepartureAirport(String outboundDepartureAirport) {
+        this.outboundDepartureAirport = outboundDepartureAirport;
+    }
+
+    public String getOutboundDepartureDate() {
+        return outboundDepartureDate;
+    }
+
+    public void setOutboundDepartureDate(String outboundDepartureDate) {
+        this.outboundDepartureDate = outboundDepartureDate;
+    }
+
+    public String getOutboundArrivalAirport() {
+        return outboundArrivalAirport;
+    }
+
+    public void setOutboundArrivalAirport(String outboundArrivalAirport) {
+        this.outboundArrivalAirport = outboundArrivalAirport;
+    }
+
+    public String getOutboundArrivalDate() {
+        return outboundArrivalDate;
+    }
+
+    public void setOutboundArrivalDate(String outboundArrivalDate) {
+        this.outboundArrivalDate = outboundArrivalDate;
+    }
+
+    public String getInboundDepartureAirport() {
+        return inboundDepartureAirport;
+    }
+
+    public void setInboundDepartureAirport(String inboundDepartureAirport) {
+        this.inboundDepartureAirport = inboundDepartureAirport;
+    }
+
+    public String getInboundDepartureDate() {
+        return inboundDepartureDate;
+    }
+
+    public void setInboundDepartureDate(String inboundDepartureDate) {
+        this.inboundDepartureDate = inboundDepartureDate;
+    }
+
+    public String getInboundArrivalAirport() {
+        return inboundArrivalAirport;
+    }
+
+    public void setInboundArrivalAirport(String inboundArrivalAirport) {
+        this.inboundArrivalAirport = inboundArrivalAirport;
+    }
+
+    public String getInboundArrivalDate() {
+        return inboundArrivalDate;
+    }
+
+    public void setInboundArrivalDate(String inboundArrivalDate) {
+        this.inboundArrivalDate = inboundArrivalDate;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getCarrier() {
+        return carrier;
+    }
+
+    public void setCarrier(String carrier) {
+        this.carrier = carrier;
     }
 }
